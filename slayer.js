516afc52b237f9dea4fb54bb79151a5f24d7d68f
Danny Coates <dannycoates@gmail.com>
2013-06-27 23:23:42 -0700

    rewrite, lulz

--- slayer.js
+++ slayer.js
@@ -1,24 +1,110 @@
 module.exports = function (inherits, EventEmitter) {
 
-	var asyncNoop = wrap(function noop() {})
+	function noop() {}
+	var asyncNoop = wrap(noop)
 	var identity = function (x) { return x }
 
-	function Slayer(name, fn, input, parent) {
-		EventEmitter.call(this)
-		this.name = name || '__root__'
-		this.fn = fn || asyncNoop
-		this.parent = parent || this
-		this.children = {}
+	function Graph(name, fn, input, output) {
 		this.input = input || []
-		this.output = ''
+		this.output = output || ''
+		this.nodes = {}
+		this.cb = noop
+		this.onError = done.bind(this)
+		this.onData = done.bind(this, null)
+	}
 
-		// map fn argument index to argument values
+	function start() {
+		var cb = arguments[arguments.length - 1]
+		if (typeof cb === 'function') {
+			this.cb = cb
+			var outputNode = this.node(this.output)
+			if (outputNode) {
+				outputNode.once('data', this.onData)
+			}
+		}
+		for (var i = 0; i < this.input.length; i++) {
+			this.add(this.input[i], { syncFn: identity, input: [i] })
+		}
+		this.connect()
+		for (var i = 0; i < arguments.length - 1; i++) {
+			execute.call(this.node(this.input[i]), i, arguments[i])
+		}
+	}
+
+	function makeGraph(name, spec) {
+		//TODO: option shifting
+		var root = new Graph(name, asyncNoop, spec.input, spec.output)
+		var dnodes = spec.nodes
+		var names = Object.keys(dnodes)
+		for (var i = 0; i < names.length; i++) {
+			var name = names[i]
+			var dnode = dnodes[name]
+			root.add(name, dnode)
+		}
+
+		return start.bind(root)
+	}
+
+	Graph.prototype.add = function (name, spec) {
+		var fn = asyncNoop
+		if (spec.fn) {
+			fn = spec.fn
+		}
+		else if (spec.syncFn) {
+			fn = wrap(spec.syncFn)
+		}
+		else if (spec.nodes) {
+			fn = makeGraph(name, spec)
+		}
+		this.nodes[name] = new Node(name, fn, spec.input, this)
+		return this
+	}
+
+	Graph.prototype.connect = function () {
+		var names = Object.keys(this.nodes)
+		for (var i = 0; i < names.length; i++) {
+			var n = this.nodes[names[i]]
+			n.connect()
+			n.once('error', this.onError)
+		}
+	}
+
+	Graph.prototype.disconnect = function () {
+		var names = Object.keys(this.nodes)
+		for (var i = 0; i < names.length; i++) {
+			var n = this.nodes[names[i]]
+			n.disconnect()
+			n.removeListener('error', this.onError)
+		}
+	}
+
+	Graph.prototype.node = function (name) {
+		return this.nodes[name]
+	}
+
+	function done(err, result) {
+		if (err) {
+			this.disconnect()
+		}
+		this.cb(err, result)
+		this.cb = noop
+	}
+
+	function Node(name, fn, input, graph) {
+		EventEmitter.call(this)
+		this.name = name
+		this.fn = fn
+		this.graph = graph
+		this.input = input || []
+		this.onData = noop
+
+		// fn argument values
 		this.args = []
-		// map dependant node names to fn argument indices 'foo':[0,1]
+		// map predecessor node names to fn argument indices 'foo':[0,1]
 		this.argDeps = {}
 		// number of arguments that have received a value
 		this.argsReceived = 0
-		// poplulate the keys for this.args and this.argDeps
+		// poplulate the keys for this.argDeps
 		for (var i = 0; i < this.input.length; i++) {
 			var n = this.input[i]
 			this.argDeps[n] = this.argDeps[n] || []
@@ -27,111 +113,34 @@ module.exports = function (inherits, EventEmitter) {
 		// the last arg must always be a callback(err, ...) function
 		this.args[this.input.length] = callback.bind(this)
 	}
-	inherits(Slayer, EventEmitter)
+	inherits(Node, EventEmitter)
 
-	function go() {
-		var cb = arguments[arguments.length - 1]
-		if (typeof cb === 'function') {
-			var outputNode = this.node(this.output)
-			if (outputNode) {
-				outputNode.once('data', cb.bind(this, null))
-			}
-			//TODO a global err handler
-		}
-		for (var i = 0; i < arguments.length - 1; i++) {
-			var name = this.input[i]
-			var node = this.node(name)
-			execute.call(node, name, arguments[i])
-		}
-	}
-
-	function makeRoot(name, def) {
-		var root = new Slayer(name, asyncNoop, def.input)
-
-		for (var i = 0; i < def.input.length; i++) {
-			var name = def.input[i]
-			root.buildSync(name, identity, [''])
-		}
-		root.output = def.output
-		root.fn = go.bind(root)
-		return root
-	}
-
-	Slayer.graph = function (name, def) {
-		//TODO: option shifting
-		var root = makeRoot(name, def)
-		var dnodes = def.nodes
-		var nodeNames = Object.keys(dnodes)
-		for (var i = 0; i < nodeNames.length; i++) {
-			var name = nodeNames[i]
-			var dnode = dnodes[name]
-			if (dnode.fn) {
-				root.build(name, dnode.fn, dnode.input)
-			}
-			else if (dnode.syncFn) {
-				root.buildSync(name, dnode.syncFn, dnode.input)
-			}
-			else if (dnode.nodes) {
-				root.build(name, Slayer.graph('_' + name, dnode), dnode.input)
-			}
-		}
-
-		return function () {
-			this.run(Array.prototype.slice.call(arguments))
-		}.bind(root)
-	}
-
-	Slayer.prototype._build = function (name, fn, input) {
-		var n = new Slayer(name, fn, input, this)
-		this.children[name] = n
-		return n
-	}
-
-	Slayer.prototype.build = function (name, fn, input) {
-		return this.parent._build(name, fn, input)
-	}
-
-	Slayer.prototype.buildSync = function (name, fn, input) {
-		return this.build(name, wrap(fn), input)
-	}
-
-	Slayer.prototype.node = function (name) {
-		return this.children[name]
-	}
-
-	Slayer.prototype.connect = function () {
-		var argDepNames = Object.keys(this.argDeps)
-		for (var i = 0; i < argDepNames.length; i++) {
-			var depName = argDepNames[i]
-			var node = this.parent.node(depName)
+	Node.prototype.connect = function () {
+		var names = Object.keys(this.argDeps)
+		for (var i = 0; i < names.length; i++) {
+			var node = this.graph.node(names[i])
 			if (node) {
-				node.once('data', execute.bind(this, depName))
+				this.onData = execute.bind(this, node.name)
+				node.once('data', this.onData)
 			}
 		}
-		if (argDepNames.length === 0) {
-			// functions with no arguments can run right now
-			this.run(this.args)
+		if (names.length === 0) {
+			this.run(this.args) // functions with no arguments can run right now
 		}
-		return this
 	}
 
-	Slayer.prototype.run = function (input) {
-		var childNames = Object.keys(this.children)
-		for (var i = 0; i < childNames.length; i++) {
-			this.children[childNames[i]].connect()
+	Node.prototype.disconnect = function () {
+		var names = Object.keys(this.argDeps)
+		for (var i = 0; i < names.length; i++) {
+			var node = this.graph.node(names[i])
+			if (node) {
+				node.removeListener('data', this.onData)
+			}
 		}
+	}
+
+	Node.prototype.run = function (input) {
 		this.fn.apply(null, input)
-	}
-
-	Slayer.prototype.longName = function () {
-		var name = ''
-		if (this !== this.parent) {
-			name = this.parent.longName() + ':' + this.name
-		}
-		else {
-			name = this.name
-		}
-		return name
 	}
 
 	function callback(err, data) {
@@ -140,7 +149,7 @@ module.exports = function (inherits, EventEmitter) {
 	}
 
 	function execute(arg, data) {
-		var indices = this.argDeps[arg] || [0]
+		var indices = this.argDeps[arg]
 		for (var i = 0; i < indices.length; i++) {
 			this.argsReceived++
 			this.args[indices[i]] = data
@@ -169,5 +178,5 @@ module.exports = function (inherits, EventEmitter) {
 		return wrapper
 	}
 
-	return Slayer
+	return makeGraph
 }
